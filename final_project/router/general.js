087ba543d4660265cafee9618c7a04a81076b791
Euge Pavia <eugepavia@gmail.com>
2026-05-21 20:53:24 -0400

    Add endpoint to get books by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -28,9 +28,9 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN (key considered as ISBN for display purposes)
 public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
-    let book = books[isbn]
-    if (book) {
-        return res.status(200).send(JSON.stringify(book));
+    let result = books[isbn]
+    if (result) {
+        return res.status(200).send(JSON.stringify(result));
     } else {
         return res.status(404).json({message: "Book not found"});
     }
@@ -38,8 +38,16 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    let result = Object.values(books).filter((book)=>{
+        return (book.author.toLocaleLowerCase().includes(author.toLocaleLowerCase()));
+    });
+
+    if (result.length > 0) {
+        return res.status(200).send(JSON.stringify(result));
+    } else {
+        return res.status(404).json({message: "Author not found"});
+    }
 });
 
 // Get all books based on title
