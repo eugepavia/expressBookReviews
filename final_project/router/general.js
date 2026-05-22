259726c52d41ec967e5ee6cb2da7539bab961658
Euge Pavia <eugepavia@gmail.com>
2026-05-21 21:10:50 -0400

    Add endpoint to get reviews by ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -64,10 +64,15 @@ public_users.get('/title/:title',function (req, res) {
     }
 });
 
-//  Get book review
+// Get book review based on ISBN
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    let result = books[isbn]
+    if (result) {
+        return res.status(200).send(JSON.stringify(result.reviews));
+    } else {
+        return res.status(404).json({message: "Book not found"});
+    }
 });
 
 module.exports.general = public_users;
